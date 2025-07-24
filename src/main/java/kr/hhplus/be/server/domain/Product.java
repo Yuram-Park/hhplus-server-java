2a1c8c4f35783de53ba9f5e8254649f77352d215
Yuram-Park <uram422@gmail.com>
2025-07-25 00:18:03 +0900

    domain lombok 수정

--- src/main/java/kr/hhplus/be/server/domain/Product.java
+++ src/main/java/kr/hhplus/be/server/domain/Product.java
@@ -3,12 +3,18 @@ package kr.hhplus.be.server.domain;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
 @Entity
 @Table
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Product {
 
     @Id
@@ -32,79 +38,7 @@ public class Product {
     @Temporal(TemporalType.TIMESTAMP)
     private Timestamp updatedAt;
 
-    public Product() {}
-
-    public Product(String productId, String productName, String productDescription, int productInventory, int productPrice) {
-        this.productId = productId;
-        this.productName = productName;
-        this.productDescription = productDescription;
-        this.productInventory = productInventory;
-        this.productPrice = productPrice;
-    }
-
-    public Product(String productId, int productInventory) {
-        this.productId = productId;
-        this.productInventory = productInventory;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
-    }
-
-    public int getProductInventory() {
-        return productInventory;
-    }
-
-    public void setProductInventory(int productInventory) {
-        this.productInventory = productInventory;
-    }
-
-    public int getProductPrice() {
-        return productPrice;
-    }
-
-    public void setProductPrice(int productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    public Timestamp getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Timestamp getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Timestamp updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
     // Business Logic
-
 
     public void reduceInventory(int reduceCount) {
         if(reduceCount <= 0) {
